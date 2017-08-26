8a8ebf9eebe95bf29f75df59e0c7662f49fa87a4
Syarif <ibrohim.syarif@gmail.com>
2017-08-26 15:36:05 +0700

    prime

--- prime/prime.js
+++ prime/prime.js
@@ -1,50 +1,77 @@
 $(document).ready(function(){
+            $("#result").hide();
+            $("#number").click(function(){
+                $("#result").hide();
+            });
     		$("#tes").click(function(){
-    			var result = prime(parseInt($("#number").val()));
-    			if (result==0){
-    				result = composite(parseInt($("#number").val()));
-    			}
-    			else {
-    				result = "It's Prime Number";
-    			}
-        		$("#result").text(result);
+                // alert($("#number").val());
+                $("#result").show();
+                if (parseInt($("#number").val())!=$("#number").val())
+                {
+                    $("#result").text("It's not Integer!");
+                }
+                else {
+                    var result = prime(parseInt($("#number").val()));
+
+
+                if (result==0){
+                    result = composite(parseInt($("#number").val()));
+                }
+                else {
+                    result = "It's Prime Number";
+                }
+                $("#result").text(result);
+                }
+    			
     		});
 
 
 
     		function composite(number){
-    			var result = [];
+                var a = parseInt($("#number").val());
+                if (a==1){
+                    return "It's Composite Number";
+                }
+                else if (a<=0)
+                {
+                    return "It's not Integer!"
+                }
+                else{
+                    var result = [];
+                
+                var n=number;
+                for (var i=2; i<number; i++){
+                    
+                    if (n%i==0) {
+                        result.push(i);
+                        // alert(i + " "+n + "-");
+                    }
+                    while (n%i==0) {
+                        n/=i;
+                        // alert("while");
+                    }
+                }
+
+                var a = "[ "
+                for (var i=0; i<result.length-1; i++){
+
+                    a = a + result[i] + ", ";
+                }
+                a = a + result[i] + " ]";
+                return "It's Composite Number with set of prime factor is "+a;
+   
+                }
     			
-    			var n=number;
-    			for (var i=2; i<number; i++){
-    				
-    				if (n%i==0) {
-    					result.push(i);
-    					// alert(i + " "+n + "-");
-    				}
-    				while (n%i==0) {
-    					n/=i;
-    					// alert("while");
-    				}
-    			}
-
-    			var a = "[ "
-    			for (var i=0; i<result.length-1; i++){
-
-    				a = a + result[i] + ", ";
-    			}
-    			a = a + result[i] + " ]";
-    			return "It's Composite Number with set of prime factor is "+a;
-
 
     		};
 
     		function prime(a){
     			var b = 0;
+                
     			if (a==2){
     				var result = 1;
     			}
-    			else if (a%2==0){
+    			else if (a%2==0 || a==1 || a<=0 ){
     				var result = 0;
     			}
     			else {
